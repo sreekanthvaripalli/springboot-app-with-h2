0864d7759aa28b11f79db7bf55a8a4bc0fc5bf6e
sreekanth <sreekanthvaripalli@gmail.com>
2021-01-24 01:43:02 +0800

    Mapped handler method to service

--- src/main/java/com/sree/springboot/operations_resource/OperationsHandler.java
+++ src/main/java/com/sree/springboot/operations_resource/OperationsHandler.java
@@ -1,9 +1,26 @@
 package com.sree.springboot.operations_resource;
 
+import com.sree.springboot.operations.OperationsCommandService;
+import com.sree.springboot.operations.User;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
+@Component
+@AllArgsConstructor
 public class OperationsHandler {
+
+    private OperationsCommandService operationsCommandService;
+
     public ResponseEntity<String> saveDetail(UserRequest userRequest) {
-        throw new UnsupportedOperationException("Operation not supported yet");
+        return operationsCommandService.saveUser(getUser(userRequest));
+    }
+
+    private User getUser(UserRequest userRequest) {
+        return User.builder()
+                .name(userRequest.getName())
+                .age(userRequest.getAge())
+                .address(userRequest.getAddress())
+                .build();
     }
 }
--- src/test/java/com/sree/springboot/operations_resource/OperationsHandlerTest.java
+++ src/test/java/com/sree/springboot/operations_resource/OperationsHandlerTest.java
@@ -1,12 +1,20 @@
 package com.sree.springboot.operations_resource;
 
+import com.sree.springboot.operations.OperationsCommandService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OperationsHandlerTest {
@@ -14,13 +22,25 @@ public class OperationsHandlerTest {
     @InjectMocks
     private OperationsHandler operationsHandler;
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testShouldThrowUnsupportedOperationException(){
+    @Mock
+    private OperationsCommandService operationsCommandService;
+
+    @Test
+    public void testShouldSaveUser(){
         //Given
+        UserRequest userRequest = UserRequest.builder()
+                .name("sree")
+                .age(27)
+                .address("KKD")
+                .build();
+        Optional<String> body = Optional.of("Success");
+        when(operationsCommandService.saveUser(any())).thenReturn(ResponseEntity.of(body));
 
         //When
-        ResponseEntity<String> stringResponseEntity = operationsHandler.saveDetail(mock(UserRequest.class));
+        ResponseEntity<String> stringResponseEntity = operationsHandler.saveDetail(userRequest);
 
         //Then
+        assertNotNull(stringResponseEntity);
+        assertEquals(body.get(), stringResponseEntity.getBody());
     }
 }
