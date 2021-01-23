dfb046e2b02dcd5c8048dc1676811e3b62838427
sreekanth <sreekanthvaripalli@gmail.com>
2021-01-24 00:42:45 +0800

    Added driving side related files: controller, requestbody and handler

---
+++ src/main/java/com/sree/springboot/operations_resource/OperationsHandler.java
@@ -0,0 +1,9 @@
+package com.sree.springboot.operations_resource;
+
+import org.springframework.http.ResponseEntity;
+
+public class OperationsHandler {
+    public ResponseEntity<String> saveDetail(UserRequest userRequest) {
+        throw new UnsupportedOperationException("Operation not supported yet");
+    }
+}
