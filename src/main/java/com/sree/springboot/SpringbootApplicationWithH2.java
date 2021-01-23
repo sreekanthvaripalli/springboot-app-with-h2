01a3c8970bf64ae5ea3f6b04eb3b051426ebe8b9
sreekanth <sreekanthvaripalli@gmail.com>
2021-01-23 20:24:31 +0800

    Initital commit

---
+++ src/main/java/com/sree/springboot/SpringbootApplicationWithH2.java
@@ -0,0 +1,13 @@
+package com.sree.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationWithH2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplicationWithH2.class, args);
+	}
+
+}
