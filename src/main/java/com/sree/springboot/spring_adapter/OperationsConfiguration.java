0483501074251f9664eba19d4c97f2b5aa5be60c
Sreekanth <sreekanthvaripalli@gmail.com>
2021-02-08 00:44:34 +0800

    Added new bean for Query Service

--- src/main/java/com/sree/springboot/spring_adapter/OperationsConfiguration.java
+++ src/main/java/com/sree/springboot/spring_adapter/OperationsConfiguration.java
@@ -3,6 +3,9 @@ package com.sree.springboot.spring_adapter;
 import com.sree.springboot.operations.OperationsCommandRepository;
 import com.sree.springboot.operations.OperationsCommandService;
 import com.sree.springboot.operations.OperationsCommandServiceAdapter;
+import com.sree.springboot.operations.OperationsQueryRepository;
+import com.sree.springboot.operations.OperationsQueryService;
+import com.sree.springboot.operations.OperationsQueryServiceAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,14 @@ public class OperationsConfiguration {
     @Bean
     public OperationsCommandService operationsCommandService(
             OperationsCommandRepository operationsCommandRepository
-    ){
+    ) {
         return new OperationsCommandServiceAdapter(operationsCommandRepository);
+    }
+
+    @Bean
+    public OperationsQueryService operationsQueryService(
+            OperationsQueryRepository operationsQueryRepository
+    ) {
+        return new OperationsQueryServiceAdapter(operationsQueryRepository);
     }
 }
