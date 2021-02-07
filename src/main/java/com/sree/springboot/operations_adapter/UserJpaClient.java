ded0e75917010d2eb07403e707c34949cdb568a1
Sreekanth <sreekanthvaripalli@gmail.com>
2021-02-08 00:41:12 +0800

    updated jpa client to fetch data from DB with native query

--- src/main/java/com/sree/springboot/operations_adapter/UserJpaClient.java
+++ src/main/java/com/sree/springboot/operations_adapter/UserJpaClient.java
@@ -2,8 +2,17 @@ package com.sree.springboot.operations_adapter;
 
 import com.sree.springboot.operations.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserJpaClient extends JpaRepository<User, Long> {
+
+    @Query(
+            value = "SELECT * FROM USERDETAIL WHERE USERDETAIL.name = ?1",
+            nativeQuery = true
+    )
+    List<User> findUserByName(String userName);
 }
