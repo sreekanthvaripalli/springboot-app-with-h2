1d20b9b4ad1795d37f5302e2c341a757e0540681
sreekanth <sreekanthvaripalli@gmail.com>
2021-02-07 15:40:13 +0800

    updated model to receive values picked from DB

--- src/main/java/com/sree/springboot/operations/User.java
+++ src/main/java/com/sree/springboot/operations/User.java
@@ -3,18 +3,38 @@ package com.sree.springboot.operations;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @AllArgsConstructor
 @Getter
 @Builder
+@Entity
+@NoArgsConstructor
+@Table(name = "USERDETAIL")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "age")
     private Integer age;
+
+    @Column(name = "address")
     private String address;
-    private String passportNumber;
 
     @Override
     public String toString(){
-        return String.format("User : [name: %s, age: %s, address: %s, passportNumber: %s]", name, age, address, passportNumber);
+        return String.format("User : [name: %s, age: %s, address: %s, passportNumber: %s]", name, age, address);
     }
 }
