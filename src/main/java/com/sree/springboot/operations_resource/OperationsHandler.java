6dc170cc23e8c7f9bc3ba9d43ceee4dabc7531ea
Sreekanth <sreekanthvaripalli@gmail.com>
2021-02-08 00:45:35 +0800

    updated handler to fetch data from core

--- src/main/java/com/sree/springboot/operations_resource/OperationsHandler.java
+++ src/main/java/com/sree/springboot/operations_resource/OperationsHandler.java
@@ -1,8 +1,10 @@
 package com.sree.springboot.operations_resource;
 
 import com.sree.springboot.operations.OperationsCommandService;
+import com.sree.springboot.operations.OperationsQueryService;
 import com.sree.springboot.operations.User;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class OperationsHandler {
 
     private OperationsCommandService operationsCommandService;
+    private OperationsQueryService operationsQueryService;
 
     public ResponseEntity<String> saveDetail(UserRequest userRequest) {
         return operationsCommandService.saveUser(getUser(userRequest));
@@ -25,6 +28,17 @@ public class OperationsHandler {
     }
 
     public ResponseEntity<UserResponse> getUser(String userName) {
-        return null;
+        ResponseEntity<User> user = operationsQueryService.getUser(userName);
+        ResponseEntity<?> responseEntity = user.getStatusCodeValue() == 200 ? getMappedResponse(user.getBody()) : user;
+        return (ResponseEntity<UserResponse>) responseEntity;
+    }
+
+    private ResponseEntity<UserResponse> getMappedResponse(User user) {
+
+        return new ResponseEntity<UserResponse>(UserResponse.builder()
+                .name(user.getName())
+                .age(user.getAge())
+                .address(user.getAddress())
+                .build(), HttpStatus.OK);
     }
 }
