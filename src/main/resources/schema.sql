52e8e23b51ddf060f4b4a0b6727dda00db818ed5
sreekanth <sreekanthvaripalli@gmail.com>
2021-02-07 15:39:25 +0800

    added data source details in application props and schema sql file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE USERDETAIL(
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    age INT(10) NOT NULL ,
+    address VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY(id)
+    );
+--
+-- insert into user (name, age, address, passportNumber) values
+-- ('sreekanth', 27, 'KKD', 'P12345'),('elvie', 27, 'PP', 'P12344');
