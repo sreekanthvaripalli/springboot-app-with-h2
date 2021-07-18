bf564a40f852337404cdc2e53bbdffd879ae3d9a
Sreekanth <sreekanthvaripalli@gmail.com>
2021-07-18 17:06:29 +0800

    Added new method to fetch all users in service and respective implementation in service adapter

--- src/main/java/com/sree/springboot/operations/OperationsQueryService.java
+++ src/main/java/com/sree/springboot/operations/OperationsQueryService.java
@@ -2,6 +2,9 @@ package com.sree.springboot.operations;
 
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface OperationsQueryService {
     ResponseEntity<User> getUser(String userName);
+    ResponseEntity<List<User>> getAllUsers();
 }
