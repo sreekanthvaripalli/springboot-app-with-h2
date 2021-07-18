7106c5af8e2c063802147076c94ea1821021cd28
Sreekanth <sreekanthvaripalli@gmail.com>
2021-07-18 17:05:26 +0800

    Added a new method in repository to fetch all users from DB and respective implementation

--- src/main/java/com/sree/springboot/operations/OperationsQueryRepository.java
+++ src/main/java/com/sree/springboot/operations/OperationsQueryRepository.java
@@ -2,6 +2,10 @@ package com.sree.springboot.operations;
 
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface OperationsQueryRepository {
     ResponseEntity<User> getUser(String userName);
+
+    ResponseEntity<List<User>> getAllUsers();
 }
